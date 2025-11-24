1a315e7d87e954b569930fc42698d0b9b9327074
huangbinbin <huangbinbin@roboscience.ai>
2025-11-24 10:25:25 +0800

    feat - module can attach qml engine and qt app

--- include/prism/qt/modular/interfaces/intf_module.h
+++ include/prism/qt/modular/interfaces/intf_module.h
@@ -1,6 +1,11 @@
 #ifndef PRISM_QT_MODULAR_INTFMODULE_H
 #define PRISM_QT_MODULAR_INTFMODULE_H
 #include "../prism_qt_modular_global.h"
+#include <prism/container.hpp>
+#include <QQmlApplicationEngine>
+#include <QVariant>
+#include <QGuiApplication>
+#include <QDebug>
 #include <QObject>
 
 namespace prism::qt::modular {
@@ -11,11 +16,24 @@ class PRISM_QT_MODULAR_EXPORT intfModule : public QObject
     virtual ~intfModule()
     {
     }
-    virtual bool register_types() = 0;
-    virtual bool init() = 0;
-    virtual bool install() = 0;
-    virtual bool uninstall() = 0;
-    virtual bool uninit() = 0;
+    Q_INVOKABLE virtual bool register_types() = 0;
+    Q_INVOKABLE virtual bool init() = 0;
+    Q_INVOKABLE virtual bool install() = 0;
+    Q_INVOKABLE virtual bool uninstall() = 0;
+    Q_INVOKABLE virtual bool uninit() = 0;
+    Q_INVOKABLE void attach_engine(QQmlApplicationEngine* engine)
+    {
+        std::shared_ptr<QQmlApplicationEngine> engine_ptr(engine, [](QQmlApplicationEngine*) {
+        });
+        prism::Container::get()->register_instance<QQmlApplicationEngine>(engine_ptr);
+
+    }
+    Q_INVOKABLE  void attach_app(QGuiApplication* app)
+    {
+        std::shared_ptr<QGuiApplication> app_ptr(app, [](QGuiApplication*) {
+        });
+        prism::Container::get()->register_instance<QGuiApplication>(app_ptr);
+    }
 };
 } // namespace prism::qt::modular
 
